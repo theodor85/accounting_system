5fd86b62cd74b52b1158397ed1789a0c85f5bb4b
Fedor Kosolapov <fedor_coder@mail.ru>
2021-04-07 19:14:56 +0700

    fix stylesby rubocop

--- rails-backend/metadata_objects/reference.rb
+++ rails-backend/metadata_objects/reference.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require 'pg'
 require 'json'
 
+# Класс для работы со справочниками
 class Reference
   def initialize(ref_name)
     @ref_name = ref_name.downcase
@@ -8,20 +11,20 @@ class Reference
       dbname: ENV['POSTGRES_DB_TEST'],
       user: ENV['POSTGRES_USER'],
       password: ENV['POSTGRES_PASSWORD_TEST'],
-      host: ENV['POSTGRES_HOST'],
+      host: ENV['POSTGRES_HOST']
     )
     @fields = []
   end
 
   def create
     query = "
-      SELECT 
+      SELECT
         create_reference('#{@ref_name}'::text, '#{@fields.to_json}'::json);
     "
     @connection.exec(query)
   end
 
   def add_field(name:, type:)
-    @fields << {:name => name, :type => type}
+    @fields << { name: name, type: type }
   end
 end
