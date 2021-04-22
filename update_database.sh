602b3e025acebee9bc5454ad34605d4d72c6ee0f
Fedor Kosolapov <fedor_coder@mail.ru>
2021-04-22 12:53:42 +0700

    change tests, add database helper;
    add createing and removing test database

--- update_database.sh
+++ update_database.sh
@@ -2,6 +2,3 @@
 
 docker-compose exec postgres psql -U debug -h localhost -d acc_sys -f /stored_procedures/system.sql
 docker-compose exec postgres psql -U debug -h localhost -d acc_sys -f /stored_procedures/references.sql
-
-docker-compose exec postgres psql -U debug -h localhost -d acc_sys_test -f /stored_procedures/system.sql
-docker-compose exec postgres psql -U debug -h localhost -d acc_sys_test -f /stored_procedures/references.sql
