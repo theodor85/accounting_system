2fa1602261da6dc501eeb1e2f47e5cadbd4e2313
Teddy Coder <fedor_coder@mail.ru>
2021-06-23 11:13:08 +0700

    add e2e test for reference create

--- rails-backend/spec/features/refs_e2e_spec.rb
+++ rails-backend/spec/features/refs_e2e_spec.rb
@@ -2,10 +2,31 @@ require 'capybara/rspec'
 
 
 RSpec.describe 'References on user interface testing: ', type: :feature, js: true do
-  it 'creating reference' do
+  it 'should have necessary headers' do
     visit 'http://rails:3000/conf'
 
     expect(page).to   have_content('Конфигуратор')
     expect(page).to   have_content('Справочники')
+  end
+
+  it 'can create reference' do
+    visit 'http://rails:3000/conf'
+
+    find_button('add_ref').click
+    # появляются поля для ввода параметров справочника
+    expect(page).to   have_content('Новый справочник')
+    expect(page).to   have_content('Имя справочника:')
+    page.fill_in 'ref_name', with: 'Test reference'
+
+    expect(page).to   have_content('Название поля:')
+    page.fill_in 'field_name', with: 'customer'
+  
+    expect(page).to   have_content('Тип поля:')
+    page.fill_in 'field_type', with: 'string'
+
+    expect(page).to   have_link('Добавить')
+    page.click_button 'Сохранить'
+
+    expect(page).to   have_content('Test reference')
   end
 end
