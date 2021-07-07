ab8e831b56cf07117651d952e074c9bdc2f8f8ab
Teddy Coder <fedor_coder@mail.ru>
2021-07-07 15:07:58 +0700

    Add form for creating ref by stimulus reflex

--- rails-backend/app/reflexes/new_ref_form_reflex.rb
+++ rails-backend/app/reflexes/new_ref_form_reflex.rb
@@ -1,5 +1,14 @@
 class NewRefFormReflex < ApplicationReflex
   def get
-    @new_ref_form = render partial: 'new_ref'
+    @reject = ActiveModel::Type::Boolean.new.cast(element.dataset[:reject])
+    if @reject
+      @button_text = 'Добавить справочник'
+      @new_ref_form = ''
+      @reject = false
+    else
+      @button_text = 'Отменить добавление'
+      @new_ref_form = render partial: 'new_ref'
+      @reject = true
+    end
   end
 end
